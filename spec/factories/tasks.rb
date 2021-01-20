a9e5863a557b44feb3c4a98ac8eed468fa724947
yoshi-sho-0606 <yoshioka0606@outlook.jp>
2021-01-20 15:47:09 +0900

    Fix: config.include...追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     user_id {1}
-    title {''}
-    status {''}    
+    title {'title'}
+    status {1}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it "is invalid without a title" do
-       task = FactoryBot.build(:task)
+       task = FactoryBot.build(:task, title: '')
        task.save
        expect(task.errors[:title]).to include("can't be blank")
 
   end
 
   it "is invalid without a status" do
-       task = FactoryBot.build(:task)
+       task = FactoryBot.build(:task, status: nil )
        task.save
        expect(task.errors[:status]).to include("can't be blank")
   end
